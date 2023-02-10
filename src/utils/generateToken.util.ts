a796c5b45576809aa25f0bbd217722999a622593
roxyzc <roxyzc12@gmail.com>
2023-02-10 16:44:20 +0700

    update

--- src/utils/generateToken.util.ts
+++ src/utils/generateToken.util.ts
@@ -2,17 +2,17 @@ import jwt from "jsonwebtoken";
 
 const generateToken = (userId: string, email: string, nama: string, role: string) => {
   const accessToken = jwt.sign({ userId, email, nama, role }, process.env.ACCESSTOKENSECRET as string, {
-    expiresIn: "15m",
+    expiresIn: 900,
   });
   const refreshToken = jwt.sign({ userId, email, nama, role }, process.env.REFRESHTOKENSECRET as string, {
-    expiresIn: "30m",
+    expiresIn: 1800,
   });
   return Promise.resolve({ accessToken, refreshToken });
 };
 
 const generateAccessToken = (userId: string, email: string, nama: string, role: string) => {
   const accessToken = jwt.sign({ userId, email, nama, role }, process.env.ACCESSTOKENSECRET as string, {
-    expiresIn: "15m",
+    expiresIn: 900,
   });
 
   return Promise.resolve({ accessToken });
